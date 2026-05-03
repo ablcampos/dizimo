a8775f7888137d1b194e3eecb3a3808784855c69
ablcampos <121516696+ablcampos@users.noreply.github.com>
2026-05-03 10:29:14 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,2 @@
 // Configuração Global da API
-
-const API_BASE_URL = 'http://127.0.0.1:9000';
-
-//const API_BASE_URL = 'http://151.245.54.164:9000';
-const  API_BASE_URL ='https://lean-albuquerque-extraordinary-minnesota.trycloudflare.com';
+const API_BASE_URL = 'https://lean-albuquerque-extraordinary-minnesota.trycloudflare.com';
